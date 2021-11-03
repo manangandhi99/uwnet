a4c50d051bbb1c5fc20d658be62090430bb5c6c8
Manan <manangandhi99@gmail.com>
2021-11-02 23:41:14 -0700

    5.3

--- src/convolutional_layer.c
+++ src/convolutional_layer.c
@@ -232,6 +232,12 @@ matrix backward_convolutional_layer(layer l, matrix dy)
 void update_convolutional_layer(layer l, float rate, float momentum, float decay)
 {
     // TODO: 5.3
+    axpy_matrix(decay, l.w, l.dw);
+    axpy_matrix(-rate, l.dw, l.w);
+    scal_matrix(momentum, l.dw);
+    // Do the same for biases as well but no need to use weight decay on biases
+    axpy_matrix(-rate, l.db, l.b);
+    scal_matrix(momentum, l.db);
 }
 
 // Make a new convolutional layer
